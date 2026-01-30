4ef22ed764f6abead52d222744ee3a5bf10c4b6d
Max007001002 <maxxx007001002@gmail.com>
2026-01-30 04:12:00 +0300

    Подключение базы и фабрика сессий

---
+++ app/db/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
+
+from app.core.config import settings
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+engine = create_async_engine(settings.DATABASE_URL, echo=settings.DEBUG)
+async_session_maker = async_sessionmaker(
+    engine,
+    expire_on_commit=False,
+    class_=AsyncSession,
+)
